6e503616fc271c31e03c8390f27f901d660a0713
siandrev <andreev.sergey.i@yandex.ru>
2022-12-26 17:03:49 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.scss'
+import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
+import {Header} from "./components/Header/Header";
+import {TxForm} from "./components/TxForm/TxForm";
+
+function App() {
+
+  return (
+      <TonConnectUIProvider
+          manifestUrl="https://ton-connect.github.io/demo-dapp/tonconnect-manifest.json"
+          theme={THEME.DARK}
+      >
+        <div className="App">
+            <Header />
+            <TxForm />
+        </div>
+      </TonConnectUIProvider>
+  )
+}
+
+export default App
---
+++ src/components/TxForm/TxForm.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback, useState } from 'react';
+import ReactJson from 'react-json-view';
+import './style.scss';
+import {useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
+
+
+const defaultTx = {
+	validUntil: Date.now() + 1000000,
+	messages: [
+		{
+			address: '0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F',
+			amount: '20000000',
+		},
+		{
+			address: '0:E69F10CC84877ABF539F83F879291E5CA169451BA7BCE91A37A5CED3AB8080D3',
+			amount: '60000000',
+		},
+	],
+};
+
+export function TxForm() {
+	const [tx, setTx] = useState(defaultTx);
+	const wallet = useTonWallet();
+	const [tonConnectUi] = useTonConnectUI();
+
+	const onChange = useCallback((value: object) => setTx((value as { updated_src: typeof defaultTx }).updated_src), []);
+
+	return (
+		<div className="send-tx-form">
+			<h3>Configure and send transaction</h3>
+			<ReactJson src={defaultTx} theme="ocean" onEdit={onChange} onAdd={onChange} onDelete={onChange} />
+			{wallet ? (
+				<button onClick={() => tonConnectUi.sendTransaction(tx)}>
+					Send transaction
+				</button>
+			) : (
+				<button onClick={() => tonConnectUi.connectWallet()}>Connect wallet to send the transaction</button>
+			)}
+		</div>
+	);
+}
