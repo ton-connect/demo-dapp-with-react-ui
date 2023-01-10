d902a118f8fd5f49a1cb1cc2c7e801b6825253a1
siandrev <andreev.sergey.i@yandex.ru>
2023-01-10 18:13:42 +0400

    footer settings updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,37 @@
-import {Locales, Theme, THEME, useTonConnectUI} from "@tonconnect/ui-react";
+import {BorderRadius, Locales, Theme, THEME, useTonConnectUI} from "@tonconnect/ui-react";
 import './footer.scss';
+import {useEffect, useState} from "react";
 
 export const Footer = () => {
+    const [checkboxes, setCheckboxes] = useState(
+        [true, false, false, true, true, true]
+    );
+
     const [_, setOptions] = useTonConnectUI();
 
     const onLangChange = (lang: string) => {
-        setOptions({ language: lang as Locales })
+        setOptions({language: lang as Locales})
     }
 
     const onThemeChange = (theme: string) => {
-        setOptions({ theme: theme as Theme })
+        setOptions({uiPreferences: {theme: theme as Theme}})
     }
+
+    const onBordersChange = (borders: string) => {
+        setOptions({uiPreferences: {borderRadius: borders as BorderRadius}})
+    }
+
+    const onCheckboxChange = (position: number) => {
+        setCheckboxes(state => state.map((item, index) => index === position ? !item : item ));
+    }
+
+    useEffect(() => {
+        const actionValues = ['before', 'success', 'error'];
+        const modals = actionValues.map((item, index) => checkboxes[index] ? item : undefined).filter(i => i) as ("before" | "success" | "error")[];
+        const notifications = actionValues.map((item, index) => checkboxes[index + 3] ? item : undefined).filter(i => i) as ("before" | "success" | "error")[];
+
+        setOptions({ actionsConfiguration: { modals, notifications } })
+    }, [checkboxes])
 
     return <footer className="footer">
         <div>
@@ -28,6 +49,47 @@ export const Footer = () => {
                 <option value={THEME.LIGHT}>light</option>
                 <option value="SYSTEM">system</option>
             </select>
+        </div>
+
+        <div>
+            <label>borders</label>
+            <select onChange={e => onBordersChange(e.target.value)}>
+                <option value="m">m</option>
+                <option value="s">s</option>
+                <option value="none">none</option>
+            </select>
+        </div>
+
+        <div className="footer-checkbox-container">
+            <span>modals</span>
+            <label>
+                before
+                <input type="checkbox" checked={checkboxes[0]} onChange={() => onCheckboxChange(0)}/>
+            </label>
+            <label>
+                success
+                <input type="checkbox" checked={checkboxes[1]} onChange={() => onCheckboxChange(1)}/>
+            </label>
+            <label>
+                error
+                <input type="checkbox" checked={checkboxes[2]} onChange={() => onCheckboxChange(2)}/>
+            </label>
+        </div>
+
+        <div className="footer-checkbox-container">
+            <span>notifications</span>
+            <label>
+                before
+                <input type="checkbox" checked={checkboxes[3]} onChange={() => onCheckboxChange(3)}/>
+            </label>
+            <label>
+                success
+                <input type="checkbox" checked={checkboxes[4]} onChange={() => onCheckboxChange(4)}/>
+            </label>
+            <label>
+                error
+                <input type="checkbox" checked={checkboxes[5]} onChange={() => onCheckboxChange(5)}/>
+            </label>
         </div>
     </footer>
 }
