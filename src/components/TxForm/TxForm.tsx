819a526a3b5e84d0db3aeb49b831d928a348d4b2
thekiba <hr@thekiba.io>
2023-11-03 04:05:54 +0400

    fix: add testing with echo contract deployment to prevent users from losing funds during tests

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -1,41 +1,67 @@
 import React, {useCallback, useState} from 'react';
-import ReactJson from 'react-json-view';
+import ReactJson, {InteractionProps} from 'react-json-view';
 import './style.scss';
-import {useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
+import {SendTransactionRequest, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 
+// In this example, we are using a predefined smart contract state initialization (`stateInit`)
+// to interact with an "EchoContract". This contract is designed to send the value back to the sender,
+// serving as a testing tool to prevent users from accidentally spending money.
+const defaultTx: SendTransactionRequest = {
+  // The transaction is valid for 10 minutes from now, in unix epoch seconds.
+  validUntil: Math.floor(Date.now() / 1000) + 600,
+  messages: [
 
-const defaultTx = {
-	validUntil: Math.floor(Date.now() / 1000) + 600, // unix epoch seconds
-	messages: [
-		{
-			address: '0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F',
-			amount: '20000000',
-		},
-		{
-			address: '0:E69F10CC84877ABF539F83F879291E5CA169451BA7BCE91A37A5CED3AB8080D3',
-			amount: '60000000',
-		},
-	],
+    {
+      // The receiver's address.
+      address: '0:8a5a9c7b70d329be670de4e6cce652d464765114aa98038c66c3d8ceaf2d19b0',
+      // Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
+      amount: '5000000',
+      // (optional) State initialization in boc base64 format.
+      stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
+      // (optional) Payload in boc base64 format.
+      payload: 'te6ccsEBAQEADAAMABQAAAAASGVsbG8hCaTc/g==',
+    },
+
+    // Uncomment the following message to send two messages in one transaction.
+    /*
+    {
+      // Note: Funds sent to this address will not be returned back to the sender.
+      address: '0:2ecf5e47d591eb67fa6c56b02b6bb1de6a530855e16ad3082eaa59859e8d5fdc',
+      amount: toNano('0.01').toString(),
+    }
+    */
+
+  ],
 };
 
 export function TxForm() {
-	const [tx, setTx] = useState(defaultTx);
-	const wallet = useTonWallet();
-	const [tonConnectUi] = useTonConnectUI();
 
-	const onChange = useCallback((value: object) => setTx((value as { updated_src: typeof defaultTx }).updated_src), []);
+  const [tx, setTx] = useState(defaultTx);
 
-	return (
-		<div className="send-tx-form">
-			<h3>Configure and send transaction</h3>
-			<ReactJson src={defaultTx} theme="ocean" onEdit={onChange} onAdd={onChange} onDelete={onChange} />
-			{wallet ? (
-				<button onClick={() => tonConnectUi.sendTransaction(tx)}>
-					Send transaction
-				</button>
-			) : (
-				<button onClick={() => tonConnectUi.openModal()}>Connect wallet to send the transaction</button>
-			)}
-		</div>
-	);
+  const wallet = useTonWallet();
+
+  const [tonConnectUi] = useTonConnectUI();
+
+  const onChange = useCallback((value: InteractionProps) => {
+    setTx(value.updated_src as SendTransactionRequest)
+  }, []);
+
+  return (
+    <div className="send-tx-form">
+      <h3>Configure and send transaction</h3>
+
+      <ReactJson theme="ocean" src={defaultTx} onEdit={onChange} onAdd={onChange} onDelete={onChange}/>
+
+      {wallet ? (
+        <button onClick={() => tonConnectUi.sendTransaction(tx)}>
+          Send transaction
+        </button>
+      ) : (
+        <button onClick={() => tonConnectUi.openModal()}>
+          Connect wallet to send the transaction
+        </button>
+      )}
+    </div>
+  );
 }
+
