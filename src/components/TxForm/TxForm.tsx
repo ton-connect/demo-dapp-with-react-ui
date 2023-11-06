5aad8574f8b4dd42d2b438052b147793e2389a6a
thekiba <hr@thekiba.io>
2023-11-07 02:48:45 +0400

    fix: add testing with echo contract deployment to prevent users from losing funds during tests

--- src/components/TxForm/TxForm.tsx
+++ src/components/TxForm/TxForm.tsx
@@ -1,16 +1,36 @@
 import React, {useCallback, useState} from 'react';
 import ReactJson from 'react-json-view';
 import './style.scss';
-import {useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
+import {SendTransactionRequest, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 
-
-const defaultTx = {
-	validUntil: Math.floor(Date.now() / 1000) + 600, // unix epoch seconds
+// In this example, we are using a predefined smart contract state initialization (`stateInit`)
+// to interact with an "EchoContract". This contract is designed to send the value back to the sender,
+// serving as a testing tool to prevent users from accidentally spending money.
+const defaultTx: SendTransactionRequest = {
+	// The transaction is valid for 10 minutes from now, in unix epoch seconds.
+	validUntil: Math.floor(Date.now() / 1000) + 600,
 	messages: [
+
 		{
-			address: '0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F',
-			amount: '20000000',
-		}
+			// The receiver's address.
+			address: '0:8a5a9c7b70d329be670de4e6cce652d464765114aa98038c66c3d8ceaf2d19b0',
+			// Amount to send in nanoTON. For example, 0.005 TON is 5000000 nanoTON.
+			amount: '5000000',
+			// (optional) State initialization in boc base64 format.
+			stateInit: 'te6cckEBBAEAOgACATQCAQAAART/APSkE/S88sgLAwBI0wHQ0wMBcbCRW+D6QDBwgBDIywVYzxYh+gLLagHPFsmAQPsAlxCarA==',
+			// (optional) Payload in boc base64 format.
+			payload: 'te6ccsEBAQEADAAMABQAAAAASGVsbG8hCaTc/g==',
+		},
+
+		// Uncomment the following message to send two messages in one transaction.
+		/*
+    {
+      // Note: Funds sent to this address will not be returned back to the sender.
+      address: '0:2ecf5e47d591eb67fa6c56b02b6bb1de6a530855e16ad3082eaa59859e8d5fdc',
+      amount: toNano('0.01').toString(),
+    }
+    */
+
 	],
 };
 
