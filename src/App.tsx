e0ebd02878b9564b39e27a83da2168c6f790cbc9
thekiba <hr@thekiba.io>
2023-11-25 19:50:03 +0400

    chore: add ton wallet for android

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ function App() {
       <TonConnectUIProvider
           manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
           uiPreferences={{ theme: THEME.DARK }}
+          walletsListConfiguration={{
+            includeWallets: [
+              {
+                appName: "tonwallet",
+                name: "TON Wallet",
+                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+                universalLink: "https://wallet.ton.org/",
+                jsBridgeKey: "tonwallet",
+                bridgeUrl: "https://bridge.tonapi.io/bridge",
+                platforms: ["chrome", "android"]
+              }
+            ]
+          }}
           actionsConfiguration={{
               twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
           }}
