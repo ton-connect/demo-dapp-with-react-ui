c56c374be4449d33c7c5ab0122d372f01e777819
thekiba <hr@thekiba.io>
2024-02-29 02:06:28 +0400

    ore: rebuild with safepal

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,28 @@ function App() {
       <TonConnectUIProvider
           manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
           uiPreferences={{ theme: THEME.DARK }}
+          walletsListConfiguration={{
+            includeWallets: [
+              {
+                appName: "safepalwallet",
+                name: "SafePal",
+                imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+                aboutUrl: "https://www.safepal.com/download",
+                jsBridgeKey: "safepalwallet",
+                platforms: ["ios", "android", "chrome", "firefox"]
+              },
+              {
+                appName: "tonwallet",
+                name: "TON Wallet",
+                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+                universalLink: "https://wallet.ton.org/ton-connect",
+                jsBridgeKey: "tonwallet",
+                bridgeUrl: "https://bridge.tonapi.io/bridge",
+                platforms: ["chrome", "android"]
+              }
+            ]
+          }}
           actionsConfiguration={{
               twaReturnUrl: 'https://t.me/DemoDappWithTonConnectBot/demo'
           }}
