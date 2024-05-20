af89011d5159c81a629227769bf5bb24c129dc93
thekiba <hr@thekiba.io>
2024-05-20 18:31:01 +0400

    chore: update @tonconnect/ui-react to 2.0.3-beta.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
-import './App.scss'
+import './App.scss';
+import './trackers';
 import {THEME, TonConnectUIProvider} from "@tonconnect/ui-react";
+import {Footer} from "./components/Footer/Footer";
 import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
-import {Footer} from "./components/Footer/Footer";
-import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
 
 function App() {
   return (
-      <TonConnectUIProvider
-          manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
-          uiPreferences={{ theme: THEME.DARK }}
-          walletsListConfiguration={{
-            includeWallets: [
-              {
-                appName: "safepalwallet",
-                name: "SafePal",
-                imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
-                aboutUrl: "https://www.safepal.com/download",
-                jsBridgeKey: "safepalwallet",
-                platforms: ["ios", "android", "chrome", "firefox"]
-              },
-              {
-                appName: "tonwallet",
-                name: "TON Wallet",
-                imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
-                aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
-                universalLink: "https://wallet.ton.org/ton-connect",
-                jsBridgeKey: "tonwallet",
-                bridgeUrl: "https://bridge.tonapi.io/bridge",
-                platforms: ["chrome", "android"]
-              }
-            ]
-          }}
-          actionsConfiguration={{
-              twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
-          }}
-      >
-        <div className="app">
-            <Header />
-            <TxForm />
-            {/*<TonProofDemo />*/}
-            <Footer />
-        </div>
-      </TonConnectUIProvider>
+    <TonConnectUIProvider
+      manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
+      uiPreferences={{theme: THEME.DARK}}
+      walletsListConfiguration={{
+        includeWallets: [
+          {
+            appName: "safepalwallet",
+            name: "SafePal",
+            imageUrl: "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+            aboutUrl: "https://www.safepal.com/download",
+            jsBridgeKey: "safepalwallet",
+            platforms: ["ios", "android", "chrome", "firefox"]
+          },
+          {
+            appName: "tonwallet",
+            name: "TON Wallet",
+            imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+            aboutUrl: "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+            universalLink: "https://wallet.ton.org/ton-connect",
+            jsBridgeKey: "tonwallet",
+            bridgeUrl: "https://bridge.tonapi.io/bridge",
+            platforms: ["chrome", "android"]
+          }
+        ]
+      }}
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/tc_twa_demo_bot/start'
+      }}
+    >
+      <div className="app">
+        <Header/>
+        <TxForm/>
+        {/*<TonProofDemo />*/}
+        <Footer/>
+      </div>
+    </TonConnectUIProvider>
   )
 }
 
---
+++ src/trackers.ts
@@ -0,0 +1,52 @@
+import {UserActionEvent, SdkActionEvent} from "@tonconnect/ui-react";
+
+const logEvent = (scope: string): (event: Event) => void => {
+  scope = scope.startsWith('ton-connect-ui-') ? 'TonConnectUI' : 'TonConnect';
+
+  return (event: Event): void => {
+    if (!(event instanceof CustomEvent<UserActionEvent | SdkActionEvent>)) {
+      return;
+    }
+    const detail: UserActionEvent | SdkActionEvent = event.detail;
+    console.log(`${scope} Event: ${detail.type}`, detail);
+  }
+};
+
+const tonConnectUiPrefix = 'ton-connect-ui-';
+const tonConnectUiEvents = [
+  'connection-started',
+  'connection-completed',
+  'connection-error',
+  'connection-restoring-started',
+  'connection-restoring-completed',
+  'connection-restoring-error',
+  'transaction-sent-for-signature',
+  'transaction-signed',
+  'transaction-signing-failed',
+  'disconnection',
+].map(event => `${tonConnectUiPrefix}${event}`)
+
+const tonConnectPrefix = 'ton-connect-';
+const tonConnectEvents = [
+  'connection-started',
+  'connection-completed',
+  'connection-error',
+  'connection-restoring-started',
+  'connection-restoring-completed',
+  'connection-restoring-error',
+  'transaction-sent-for-signature',
+  'transaction-signed',
+  'transaction-signing-failed',
+  'disconnection',
+].map(event => `${tonConnectPrefix}${event}`)
+
+const events = [
+  ...tonConnectUiEvents,
+  ...tonConnectEvents
+];
+
+for (const event of events) {
+  try {
+    window.addEventListener(`${event}`, logEvent(event));
+  } catch (e) {}
+}
